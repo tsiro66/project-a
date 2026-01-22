0a48b99c054a4dbca9c6b16cb691f9bcbfeb4fbf
Thodoris Tsironis <theogr99@gmail.com>
2026-01-22 18:23:56 +0200

    Renamed page component folder

--- app/PageComponents/About.tsx
+++ app/PageComponents/About.tsx
@@ -3,7 +3,6 @@ import React, { useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
-import Button from "../components/Button";
 
 gsap.registerPlugin(ScrollTrigger);
 
